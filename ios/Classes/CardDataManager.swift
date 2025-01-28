60a44e6cad676114fc212ac6088387edd40d6fe5
kschnieders <karen.schnieders@jci.com>
2025-01-28 16:05:01 +0100

    WHCCareKit eingebunden

--- ios/Classes/CardDataManager.swift
+++ ios/Classes/CardDataManager.swift
@@ -1,35 +1,95 @@
-// ios/Classes/Swift/CardDataManager.swift
-import Foundation
 import WHCCareKit_iOS
 
-class CardDataManager {
+class CarddataManager {
+    private let bluetoothManager = CareBluetoothManager()
     private var taskManager: CareTaskManager?
-    
+
     init() {
-        // Initialize the task manager and other necessary components here
+        bluetoothManager.delegate = self
     }
-    
-    func createPoolForLoadVSD() {
-        // Your implementation for createPoolForLoadVSD
-    }
-    
+
     func loadVSD() {
-        // Your implementation for loadVSD
-        taskManager?.loadVSD()
+        bluetoothManager.connectToDevice(deviceIndex: 0)
+                taskManager?.loadVSD()
     }
-    
+
     func loadNFD() {
-        // Your implementation for loadNFD
-        taskManager?.loadNFD(targetEnvironment: .test, emergencyIndicator: true, updateIndicator: false)
+        taskManager?.loadNFD(targetEnvironment: HealthDeviceTargetEnvironment.productive, emergencyIndicator: false, updateIndicator: false)
     }
-    
+
     func loadDPE() {
-        // Your implementation for loadDPE
-        taskManager?.loadDPE(targetEnvironment: .test, emergencyIndicator: true, updateIndicator: false)
+        taskManager?.loadDPE(targetEnvironment: HealthDeviceTargetEnvironment.productive, emergencyIndicator: false, updateIndicator: false)
     }
-    
+
     func loadAMTS() {
-        // Your implementation for loadAMTS
-        taskManager?.loadAMTS(targetEnvironment: .test)
+        taskManager?.loadAMTS(targetEnvironment: HealthDeviceTargetEnvironment.productive)
+    }
+
+    func cleanupAndDisconnect() {
+        taskManager?.cleanup(completion: {
+            self.bluetoothManager.disconnectFromDevice()
+        })
+    }
+}
+
+extension CarddataManager: CareBluetoothManagerDelegate {
+    func careBluetoothManager(didUpdateState state: CareBluetoothManagerStates) {
+        print("careBluetoothManager didUpdateState> \(state)")
+    }
+
+    func careBluetoothManager(didThrowMessage message: CareBluetoothManagerMessages) {
+        print("careBluetoothManager didThrowMessage> \(message)")
+        if message == .newPeripheralDetected {}
+    }
+
+    func careBluetoothManager(didConnect ctapi: CTorgios) {
+        taskManager = CareTaskManager(ctapi: ctapi, delegate: self, completion: {
+            self.taskManager?.loadVSD()
+        })
+    }
+}
+
+extension CarddataManager: CareTaskManagerDelegate {
+    func careTaskManager(didUpdateState currentAction: CareTaskManagerActions) {
+        print("careTaskManager didUpdateState> \(currentAction)")
+    }
+
+    func careTaskManager(didThrowMessage message: CareTaskManagerMessages) {
+        print("careTaskManager didThrowMessage> \(message)")
+    }
+
+    func careTaskManager(didLoadVSD dataset: [Data]) {
+        print("careTaskManager didLoadVSD> datasets: \(dataset)")
+        for data in dataset {
+            print(String(decoding: data, as: UTF8.self))
+        }
+    }
+
+    func careTaskManager(didLoadVSD_KVK dataset: Data) {
+        print("careTaskManager didLoadVSD_KVK> datasets: \(dataset)")
+        print(String(decoding: dataset, as: UTF8.self))
+        taskManager?.ejectCard(slotIDs: HealthDeviceConstants.MobKT.SlotIDs.Slot1)
+    }
+
+    func careTaskManager(didLoadNFD dataset: [Data]) {
+        print("careTaskManager didLoadNFD> datasets: \(dataset)")
+        for data in dataset {
+            print(String(decoding: data, as: UTF8.self))
+        }
+        taskManager?.ejectCard(slotIDs: HealthDeviceConstants.MobKT.SlotIDs.Slot1, HealthDeviceConstants.MobKT.SlotIDs.Slot2)
+    }
+
+    func careTaskManager(didLoadDPE dataset: [Data]) {
+        print("careTaskManager didLoadDPE> datasets: \(dataset)")
+        for data in dataset {
+            print(String(decoding: data, as: UTF8.self))
+        }
+    }
+
+    func careTaskManager(didLoadAMTS dataset: [Data]) {
+        print("careTaskManager didLoadAMTS> datasets: \(dataset)")
+        for data in dataset {
+            print(String(decoding: data, as: UTF8.self))
+        }
     }
 }
